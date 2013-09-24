e778b3a081a958f8bbed72c62c5e739220159dc2
Ignacio Diez <ig.diez10@gmail.com>
2013-09-24 01:27:53 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trazapp::Application.config.secret_token = '68131bd3e1b7b84340fa9c2121f425d9b9517686aa315002254125d7fbf37b77570f23ac2e088fc01188a0c1951ef613972fcf23fffa8fd73b7ec24ee81d73a6'
