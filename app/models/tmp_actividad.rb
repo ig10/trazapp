26cda572a49e6ab5055a1ba33c5a182539364729
Ignacio Diez <ig.diez10@gmail.com>
2013-10-15 02:49:01 -0300

    Activities: Declaration of variable with ranges of complexity

--- app/models/tmp_actividad.rb
+++ app/models/tmp_actividad.rb
@@ -1,4 +1,7 @@
 class TmpActividad < ActiveRecord::Base
   belongs_to :proyecto, :class_name => 'TmpProyecto', :foreign_key => 'proyecto_id'
   attr_accessible :complejidad, :estado, :funcionalidad, :modulo, :proyecto_id, :revision
+
+  Complejidad = {"Alta" => 1,"Baja" => 2, "Media" => 3}
+
 end
