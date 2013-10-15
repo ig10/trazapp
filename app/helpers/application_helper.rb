5fb55e5d0bd88002c9c61f3db2f1aadfa1a67bd6
Ignacio Diez <ig.diez10@gmail.com>
2013-10-15 02:47:35 -0300

    AppHelper: Method to provide ES month names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def meses_helper
+    ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre','Diciembre']
+  end
 end
