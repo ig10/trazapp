8a61f66765f8e2242131c5907c441ee6b276d723
Ignacio Diez <idiez@comparaonline.com>
2013-10-15 20:13:21 -0300

    Helper: Dates helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,22 @@
 module ApplicationHelper
   def meses_helper
-    ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre','Diciembre']
+    {
+      'Enero' => 1,
+      'Febrero' => 2,
+      'Marzo' => 3,
+      'Abril' => 4,
+      'Mayo' => 5,
+      'Junio' => 6,
+      'Julio' => 7,
+      'Agosto' => 8,
+      'Septiembre' => 9,
+      'Octubre' => 10,
+      'Noviembre' => 11,
+      'Diciembre' => 12
+    }
+  end
+
+  def anio_helper
+    Time.now.year..(Time.now + 10.years).year
   end
 end
