d7926945cfc9ca0cb38c18486d0c21c6e7f1a631
Ignacio Diez <idiez@comparaonline.com>
2013-10-15 20:14:08 -0300

    TmpActividad: Date formatting callback and its attrs

--- app/models/tmp_actividad.rb
+++ app/models/tmp_actividad.rb
@@ -1,7 +1,19 @@
 class TmpActividad < ActiveRecord::Base
   belongs_to :proyecto, :class_name => 'TmpProyecto', :foreign_key => 'proyecto_id'
-  attr_accessible :complejidad, :estado, :funcionalidad, :modulo, :proyecto_id, :revision
+  attr_accessible :complejidad, :estado, :funcionalidad, :modulo, :proyecto_id, :revision, :dia, :mes, :anio
+  attr_accessor :dia, :mes, :anio
 
-  Complejidad = {"Alta" => 1,"Baja" => 2, "Media" => 3}
+  before_create :formatear_fecha
+
+  Complejidad = {"Baja" => 1,"Media" => 2, "Alta" => 3}
+
+  def formatear_fecha
+    unless dia.nil? and mes.nil? and anio.nil?
+      self.revision = [dia,mes,anio].join("-").to_time
+    else
+      logger.error "\033[31m FORMATTING DATE ERROR! \033[0m"
+    end
+  end
+
 
 end
