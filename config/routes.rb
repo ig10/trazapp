cd9886afebed52d4e2567e5794878e8d00e8bcb7
Ignacio Diez <ig.diez10@gmail.com>
2013-10-28 13:07:44 -0300

    Specific route for projet edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Trazapp::Application.routes.draw do
 
   match '/alumnos' => 'tmp_proyectos#new'
   match '/profesor' => 'tmp_proyectos#index'
-  match '/proyecto/editar/:id' => 'tmp_proyectos#edit'
   match '/tmp_proyectos' => 'tmp_proyectos#create'
-
+  match '/proyecto/editar/:id' => 'tmp_proyectos#edit'
+  match '/tmp_proyectos/:id' => 'tmp_proyectos#update'
 end
