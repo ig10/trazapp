c27f6a4aaed271a936e8d6d4772fc1fc90c35604
Ignacio Diez <idiez@comparaonline.com>
2013-10-29 19:50:00 -0300

    Routes associated to new models

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,36 @@
 Trazapp::Application.routes.draw do
   resources :tmp_actividades
   resources :tmp_proyectos
+  resources :proyectos
   resources :usuarios
 
   root to: 'public#index'
 
   match '/alumnos' => 'tmp_proyectos#new'
+
+
+  # Modify this routes considering new views in Teacher's admin Panel
   match '/profesor' => 'tmp_proyectos#index'
+
+  match '/solicitudes' => 'tmp_proyectos#index'
+  match '/activos' => 'proyectos#index'
+
   match '/tmp_proyectos' => 'tmp_proyectos#create'
-  match '/proyecto/editar/:id' => 'tmp_proyectos#edit'
   match '/tmp_proyectos/:id' => 'tmp_proyectos#update'
+
+  scope 'solicitud' do
+    match 'nuevo' => 'tmp_proyectos#new'
+    match 'editar/:id/:codigo_acceso' => 'tmp_proyectos#edit'
+    match 'eliminar/:id/:codigo_acceso' => 'tmp_proyectos#destroy'
+    match 'aprobar/:id/:codigo_acceso' => 'tmp_proyectos#aprobar'
+  end
+
+  scope 'proyecto' do
+    match 'nuevo' => 'proyectos#new'
+    match 'editar/:id/:codigo_acceso' => 'proyectos#edit'
+    match 'eliminar/:id' => 'proyectos#destroy'
+  end
+
+  #Enviar Mail Codigo Acceso
+  match '/enviar-codigo' => 'application#enviar_codigo'
 end
