116e32b0be7eac8bfe66b175fcd33822bf3283c5
Ignacio Diez <idiez@comparaonline.com>
2013-10-29 19:51:37 -0300

    TmpProject changes to aprove and clone them to active Projects

--- app/models/tmp_proyecto.rb
+++ app/models/tmp_proyecto.rb
@@ -5,5 +5,14 @@ class TmpProyecto < ActiveRecord::Base
   delegate :rut, :nombre_completo, :correo_electronico, to: :alumnos
 
   validates_presence_of :nombre
+  before_save :formatear_nombre
+
+  def formatear_nombre
+    self.nombre = self.nombre.titleize
+  end
+
+  def codigo_acceso
+    Digest::SHA1.hexdigest("RxO39kysE#{id}uoYx#{created_at.strftime('%H%S')}LwQ4r7Di9hOV")[2..10]
+  end
 
 end
