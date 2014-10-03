6911e0538866e9a0d1c6ac0a191d4a21eb31ce76
Ignacio Diez <ig.diez10@gmail.com>
2014-10-02 21:38:13 -0300

    Removed required emial on user creation

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,7 +3,7 @@ class Usuario < ActiveRecord::Base
   belongs_to :proyecto, :class_name => 'TmpProyecto', :foreign_key => 'proyecto_id'
   attr_accessible :activo, :carrera, :correo_electronico, :nombre_completo, :perfil, :rut, :sede, :proyecto_id
 
-  validates_presence_of :rut, :correo_electronico, :nombre_completo
+  validates_presence_of :rut, :nombre_completo
 
   scope :con_rut, lambda{ |rut| where(rut: rut) unless rut.blank? }
   scope :de_sede, lambda{ |sede| where(sede: sede) unless sede.blank? }
