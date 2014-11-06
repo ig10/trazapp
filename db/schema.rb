d513f84df530ad14f53ec2a87fc3073f22a2fbcd
Daniel Pena <dnl.penaa@gmail.com>
2014-11-05 22:47:55 -0400

    Migration: added password to usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141101002421) do
+ActiveRecord::Schema.define(:version => 20141106023125) do
 
   create_table "actividades", :force => true do |t|
     t.integer  "proyecto_id"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(:version => 20141101002421) do
     t.boolean  "activo"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
+    t.string   "password"
   end
 
 end
