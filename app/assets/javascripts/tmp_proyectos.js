bb53d0faabc09c2d3a1511b432d6f06d6c2b54aa
Ignacio Diez <ig.diez10@gmail.com>
2014-11-05 22:04:10 -0300

    JS: Main functions to handle Add & Remove students

--- app/assets/javascripts/tmp_proyectos.js
+++ app/assets/javascripts/tmp_proyectos.js
@@ -1,52 +1,67 @@
 var TmpProyecto = (function(){
   function init(){
+    sectionsListener();
+    addStudentTrigger();
+    removeStudentTrigger();
+    fakeLinksEvent();
+  }
+
+  // Add & Remove Students from Select to List and inverse
+
+  function sectionsListener() {
+    // Controls change in sections to load students in selector
     $('#secciones').on('change', function() {
       loadStudents($(this).val());
+      cleanListBox();
     });
-
-    addStudentTrigger();
-
-    $('a').on('click', function(e){
-      if($(this).attr('href') == "#"){
-        e.preventDefault();
-      }
-    });
-  }
-
-  function loadStudents(selected) {
-    var json = JSON.parse($('#alumnos_json').val());
-    var studentSelector = $('.students-list');
-    var tmpl = "<option value='id'>nombre_completo</option>";
-    var selected = json[selected];
-    removeStudents();
-    $.each(selected, function(i, e) {
-      var optionBase = tmpl;
-      studentSelector.append($.parseHTML(optionBase.replace(/id/g, e[0]).replace(/nombre_completo/g, e[1])));
-    });
-  }
-
-  function removeStudents() {
-    var studentSelector = $('.students-list');
-    studentSelector.empty().append($.parseHTML("<option>Seleccione alumno</option>"));
   }
 
   function addStudentTrigger() {
+    // Controls events from button 'Agregar'
     $('.btn.add').on('click', function(e) {
       e.preventDefault();
       addStudent();
     })
   }
 
-  function addStudent() {
+  function removeStudentTrigger() {
+    // Listen in ListBox every element to be removed by the X
+    var listBox = $('.lista');
+    var deleteButton = $('.delete-student');
+
+    listBox.on('click', deleteButton, function(event) {
+      removeFromList(event.target);
+    });
+  }
+
+  function loadStudents(selected) {
+    // Loads all the students in the selector from the JSON
+    var json = JSON.parse($('#alumnos_json').val());
+    var selected = json[selected];
+    clearStudentsSelector();
+    $.each(selected, function(i, student) {
+      appendStudentToSelector(student);
+    });
+  }
+
+  function clearStudentsSelector() {
+    // Resets student selector, removing its elements
     var studentSelector = $('.students-list');
-    var tmpl = "<span data-id='id-alumno'>nombre</span>";
+    studentSelector.empty().append($.parseHTML("<option value=''>Seleccione alumno</option>"));
+  }
+
+  function addStudent() {
+    // Adds a selected student to the List
+    var studentSelector = $('.students-list');
+    var tmpl = "<span>nombre <span class='delete-student' data-id='id-alumno'>X</span></span>";
     var listBox = $('.lista');
     var student = searchStudent(studentSelector.val());
-
+    removeFromSelector(student);
     listBox.append($.parseHTML(tmpl.replace(/id-alumno/g, student[0]).replace(/nombre/g, student[1])));
   }
 
   function searchStudent(id) {
+    // Retreives an Array of the specific student
     var json = JSON.parse($('#alumnos_json').val());
     var selected = json[$('#secciones').val()];
     var found;
@@ -58,6 +73,64 @@ var TmpProyecto = (function(){
 
     return found;
   }
+
+  function removeFromSelector(student) {
+    // Removes an option from student selector by its value
+    $(".students-list option[value='"+ student[0] +"']").remove();
+  }
+
+  function removeFromList(element) {
+    // Removes a student from the list when X pressed
+    var $element = $(element);
+    studentAsOption($element.data('id'));
+    $element.parent().remove();
+  }
+
+  function studentAsOption(id) {
+    // Re-add a removed specific student to the selector
+    var student = searchStudent(id);
+    var studentSelector = $('.students-list');
+    appendStudentToSelector(student);
+    sortSelector(studentSelector);
+  }
+
+  function sortSelector(selector) {
+    // Orders the values from a selector from min to max from its text
+    var selectOptions = selector.find('option[value!=""]');
+    selectOptions.sort(function(a, b) {
+      var aName = $(a).text();
+      var bName = $(b).text();
+      return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0));
+    });
+
+    clearStudentsSelector();
+    selector.append(selectOptions).find('option').first().attr('selected', true);
+  }
+
+  function appendStudentToSelector(student) {
+    // Function to add an student to the selector list
+    var tmpl = "<option value='id'>nombre_completo</option>";
+    var studentSelector = $('.students-list');
+    studentSelector.append($.parseHTML(tmpl.replace(/id/g, student[0]).replace(/nombre_completo/g, student[1])));
+  }
+
+  function cleanListBox() {
+    // Resets Listbox to its default state
+    $('.lista').empty();
+  }
+
+  // MISC
+
+  function fakeLinksEvent() {
+    // Cancels click event on links with href eql to #
+    $('a').on('click', function(e){
+      if($(this).attr('href') == "#"){
+        e.preventDefault();
+      }
+    });
+  }
+
+
 
   function profesor(){
     $('a').on('click', function(e){
