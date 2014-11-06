6f8bb96dc15a377daedc56d1e6c7cfe4af185081
Daniel Pena <dnl.penaa@gmail.com>
2014-11-05 22:48:57 -0400

    Attr accessor to password in model Usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,7 +2,7 @@ class Usuario < ActiveRecord::Base
   belongs_to :solicitud, class_name: 'TmpProyecto', foreign_key: 'proyecto_id'
   belongs_to :proyecto, :class_name => 'TmpProyecto', :foreign_key => 'proyecto_id'
   has_and_belongs_to_many :secciones
-  attr_accessible :activo, :carrera, :correo_electronico, :nombre_completo, :perfil, :rut, :sede, :proyecto_id
+  attr_accessible :activo, :carrera, :correo_electronico, :nombre_completo, :perfil, :rut, :sede, :proyecto_id, :password
 
   validates_presence_of :rut, :nombre_completo
 
