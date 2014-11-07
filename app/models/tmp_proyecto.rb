301e9ae2c8c72f9bdc78e2e2813a2b08e1deacab
Ignacio Diez <ig.diez10@gmail.com>
2014-11-06 22:37:50 -0300

    Added description as a field in TmpProyecto

--- app/models/tmp_proyecto.rb
+++ app/models/tmp_proyecto.rb
@@ -1,10 +1,12 @@
 class TmpProyecto < ActiveRecord::Base
   has_many :alumnos, :class_name => 'Usuario', :foreign_key => 'proyecto_id'
   has_many :actividades, :class_name => 'TmpActividad', :foreign_key => 'proyecto_id'
-  attr_accessible :estado, :nombre
+  belongs_to :seccion, class_name: 'Seccion', foreign_key: 'seccion_id'
+  attr_accessible :estado, :nombre, :descripcion
   delegate :rut, :nombre_completo, :correo_electronico, to: :alumnos
 
-  validates_presence_of :nombre
+  validates_presence_of :nombre, :descripcion
+
   before_save :formatear_nombre, :expirar
 
   def formatear_nombre
