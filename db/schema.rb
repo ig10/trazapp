672ce557d172e8b3bf92ec513378d41b7502853f
Daniel Pena <dnl.penaa@gmail.com>
2014-11-18 20:08:17 -0400

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141101002421) do
+ActiveRecord::Schema.define(:version => 20141106221005) do
 
   create_table "actividades", :force => true do |t|
     t.integer  "proyecto_id"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(:version => 20141101002421) do
     t.datetime "updated_at",                                :null => false
     t.date     "fecha_expiracion"
     t.date     "expire_date"
+    t.text     "descripcion"
   end
 
   create_table "usuarios", :force => true do |t|
@@ -104,6 +105,7 @@ ActiveRecord::Schema.define(:version => 20141101002421) do
     t.boolean  "activo"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
+    t.string   "password"
   end
 
 end
