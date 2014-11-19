6eb48a676a602b5b60160702c3f09df5d2c98f21
Ignacio Diez <ig.diez10@gmail.com>
2014-11-19 20:43:05 -0300

    Required jquery validate in JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,8 @@
 //= require jquery
 //= require jquery_ujs
+//= require jquery.validate
+//= require jquery.validate.additional-methods
+//= require jquery.validate.localization/messages_es
 //= require twitter/bootstrap
 //= require highcharts
 //= require_tree .
