672ebcd14f39f639d5235f419774b24c52dba36e
Ignacio Diez <ig.diez10@gmail.com>
2014-11-19 20:52:55 -0300

    Extended for custom jQuery validator messages

---
+++ vendor/assets/javascripts/jquery.validate.messages_es.js
@@ -0,0 +1,17 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: ES
+ */
+jQuery.extend(jQuery.validator.messages, {
+  required: 'Requerido',
+  non_dummy_rut: 'RUT Inválido',
+  distinct_digits_rut: 'RUT Inválido',
+  valida_rut: "RUT Inválido",
+  non_dummy_email: 'Inválido',
+  non_dummy_name: 'Inválido',
+  valida_email:'Inválido',
+  phone_number: "Inválido",
+  distinct_alphanum: "Inválido",
+  distinct_digits: "Inválido",
+  invalid: 'Inválido'
+});
