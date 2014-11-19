1de2ab7b2109195ae76f4f96614743c4884fe39e
Ignacio Diez <ig.diez10@gmail.com>
2014-11-19 20:52:55 -0300

    Extended for custom jQuery validator messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,7 @@
 //= require jquery.validate
 //= require jquery.validate.additional-methods
 //= require jquery.validate.localization/messages_es
+//= require jquery.validate.messages_es
 //= require twitter/bootstrap
 //= require highcharts
 //= require_tree .
