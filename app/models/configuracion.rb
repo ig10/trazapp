a3d77d5319a8751b8394b3b36f82d2b87e073f39
Ignacio Diez <ig.diez10@gmail.com>
2014-12-09 21:38:34 -0300

    Configuraciones, status of the upload

--- app/models/configuracion.rb
+++ app/models/configuracion.rb
@@ -1,14 +1,16 @@
 class Configuracion < ActiveRecord::Base
   has_one :usuario, class_name: 'Usuario', foreign_key: :autor
-  attr_accessible :nombre_archivo, :autor, :formato, :created_at, :updated_at
+  attr_accessible :nombre_archivo, :autor, :formato, :created_at, :updated_at, :status
 
+  STATUS = {0 => 'complete', 1 => 'incomplete', 2 => 'unknown'}
 
-  def self.cargar(archivo)
+  def self.cargar(archivo, autor, status = 0)
     nombre, formato = archivo.original_filename.split('.')
     create(
       nombre_archivo: nombre,
       formato: formato,
-      autor: 0
+      autor: autor,
+      status: status
       )
   end
 
